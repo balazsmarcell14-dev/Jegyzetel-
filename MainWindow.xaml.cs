db567911c60a93acb476231bf422ca63837a909a
balazsmarcell14-dev <balazsmarcell14@gmail.com>
2026-04-06 11:10:50 +0200

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace jegyzetelo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private TextWriterAndReader _textWriterAndReader = new TextWriterAndReader();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            _textWriterAndReader.Save(tbContent.Text);
+
+        }
+
+
+        private void tbContent_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            _textWriterAndReader.loadFile(namingFile.Text);
+        }
+        
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            namingFile.Visibility = Visibility.Visible;
+            saveNameButton.Visibility = Visibility.Visible;
+        }
+
+        private void saveNameButton_Click(object sender, RoutedEventArgs e)
+        {
+            _textWriterAndReader.saveFileName(namingFile.Text);
+            namingFile.Visibility = Visibility.Collapsed;
+            saveNameButton.Visibility = Visibility.Collapsed;
+        }
+
+        private void namingFile_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void showFiles_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ TextWriterAndReader.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+
+namespace jegyzetelo
+{
+    public class TextWriterAndReader
+    {
+        string fileName;
+        string path;
+        
+        public void Save(string text)
+        {
+            path = $@"C:\Users\balaz\Documents\c#\jegyzetelő\jegyzetelo\{fileName}";
+
+            using (var file = File.CreateText(path))
+            { 
+                file.Write(text);
+            }
+        }
+
+        public void saveFileName(string name)
+        {
+            fileName = name + ".txt";
+        }
+
+        public void loadFile(string name)
+        {
+            path = $@"C:\Users\balaz\Documents\c#\jegyzetelő\jegyzetelo\{fileName}";
+            using (var file = File.OpenText(path))
+            {
+                string text = file.ReadToEnd();
+            }
+        }
+    }
+}
